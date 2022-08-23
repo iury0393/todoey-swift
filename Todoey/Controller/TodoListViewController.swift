4e126182e375a36cde2883046fc26e14f466ba83
Iury Vasconcelos <iury0393@gmail.com>
2022-08-23 13:41:33 -0300

    Criando o tableView

---
+++ Todoey/Controller/TodoListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TodoListViewController.swift
+//  Todoey
+//
+//  Created by Iury Vasconcelos on 23/08/22.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = itemArray[indexPath.row]
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+}
